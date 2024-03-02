33cbce479d099f59b3005ef334a539d57bbbfcb9
unknown <mohamed.aymn.mosalam@gmail.com>
2024-03-02 19:04:21 +0200

    added styling tests

--- json.go
+++ json.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"net/http"
 )
-// I love these helper functions 
+
+// I love these helper functions
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
 		log.Printf("Responding with 5XX error: %s", msg)
